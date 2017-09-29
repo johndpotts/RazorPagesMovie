7681b70ae4cd236295943d149e07c39bcd5286b2
John Potts <john@arlingtonchurch.com>
2017-09-28 22:14:50 -0400

    added CRUD operations for movie

--- RazorPagesMovie/Models/MovieContext.cs
+++ RazorPagesMovie/Models/MovieContext.cs
@@ -1,16 +1,11 @@
-﻿
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace RazorPagesMovie.Models
 {
     public class MovieContext : DbContext
     {
         public MovieContext(DbContextOptions<MovieContext> options)
-           : base(options)
+            : base(options)
         {
         }
 
